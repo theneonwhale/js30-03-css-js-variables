05ed2d574e24c0427c98847e603cb910b1060beb
Andrii Kylymnyk <a.kylymnyk@gmail.com>
2021-01-21 00:27:29 +0200

    Updates

---
+++ src.2b33da55.js
@@ -0,0 +1,8 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"Tnu0":[function(require,module,exports) {
+
+},{}],"u9EY":[function(require,module,exports) {
+var t=document.querySelectorAll(".controls input");function e(){var t=this.dataset.sizing||"";document.documentElement.style.setProperty("--".concat(this.name),this.value+t)}t.forEach(function(t){return t.addEventListener("click",e)}),t.forEach(function(t){return t.addEventListener("mousemove",e)});
+},{}],"Focm":[function(require,module,exports) {
+"use strict";require("./styles.css"),require("./js/variables");
+},{"./styles.css":"Tnu0","./js/variables":"u9EY"}]},{},["Focm"], null)
+//# sourceMappingURL=/js30-03-css-js-variables/src.2b33da55.js.map
